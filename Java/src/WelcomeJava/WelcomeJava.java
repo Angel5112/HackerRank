2928dedd37eca8b7e03913e64fe7003fc14e7076
angel5112 <aepatino.360@gmail.com>
2023-04-03 18:26:22 -0400

    Add Welcome to Java! Problem.

---
+++ Java/src/WelcomeJava/WelcomeJava.java
@@ -0,0 +1,14 @@
+package WelcomeJava;
+
+/**
+ * Print Hello, World. and Hello, Java. on the console
+ * */
+
+public class WelcomeJava {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
